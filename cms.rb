ba28e80959b59bc088d58de5372579b8f176c970
Amy Dripchak <rejuvenatingserendipity@gmail.com>
2023-10-11 10:29:50 -0600

    update

--- cms.rb
+++ cms.rb
@@ -2,11 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 require "sinatra/content_for"
+require "redcarpet"
 
 configure do
   use Rack::Session::Cookie, :key=>"rack.session", :path=>"/" 
   set :session_secret, SecureRandom.hex(32)
   set :erb, :escape_html => true
+end
+
+helpers do
+  def render_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(text)
+  end
 end
 
 root = File.expand_path("..", __FILE__)
@@ -27,5 +35,14 @@ get "/:filename" do
   else
     session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
+  end
+end
+
+get "/:filename.:ext" do
+  if params[:ext] == 'md'
+    render_markdown(File.read(params[:filename]))
+  else
+    status 404
+    'File not found'
   end
 end
